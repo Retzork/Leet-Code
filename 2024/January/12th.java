f8e59038883e1637aacc6fca30e8f20e895431e2
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-12 16:19:28 +0700

    Create 12th.java

---
+++ 2024/January/12th.java
@@ -0,0 +1,26 @@
+class 12th {
+    public boolean halvesAreAlike(String s) {
+        int mid = s.length() / 2;
+
+        String a = s.substring(0, mid);
+        String b = s.substring(mid);
+
+        int good = 0;
+
+        for(int i = 0; i < mid; i++){
+            if(isVowel(a.charAt(i))){
+                good++;
+            }
+            if(isVowel(b.charAt(i))){
+                good--;
+            }
+        }
+
+        return good == 0;
+    }
+
+    public boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}

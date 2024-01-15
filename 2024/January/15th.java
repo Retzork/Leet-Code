2c3c5eaa1136e90075fedc69713d179a8d17d1f2
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-15 18:53:43 +0700

    Create 15th.java

---
+++ 2024/January/15th.java
@@ -0,0 +1,27 @@
+class 15th {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> noLose = new ArrayList<>();
+        List<Integer> oneLose = new ArrayList<>();
+
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for(int[] match : matches){
+            count.put(match[0], count.getOrDefault(match[0], 0) + 0);
+            count.put(match[1], count.getOrDefault(match[1], 0) + 1);
+        }
+
+        count.forEach((key, value) -> {
+            if(value == 0) noLose.add(key);
+            if(value == 1) oneLose.add(key);
+        });
+
+        Collections.sort(noLose);
+        Collections.sort(oneLose);
+
+        ans.add(noLose);
+        ans.add(oneLose);
+
+        return ans;
+    }
+}

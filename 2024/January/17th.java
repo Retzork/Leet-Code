e312c81263cd101ac7388cdbbb2e6215ace1609c
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-17 07:30:36 +0700

    Create 17th.java

---
+++ 2024/January/17th.java
@@ -0,0 +1,16 @@
+class 17th {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int element : arr) {
+            countMap.put(element, countMap.getOrDefault(element, 0) + 1);
+        }
+
+        Set<Integer> uniqueValues = new HashSet<>();
+        for (Integer value : countMap.values()) {
+            if (!uniqueValues.add(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

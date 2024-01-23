3dee0774c329d34d728e170113694c11b2584015
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-23 17:11:06 +0700

    Create 21st.java

---
+++ 2024/January/21st.java
@@ -0,0 +1,20 @@
+class 21st {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+
+        if(len == 1) return nums[0];
+        if(len == 2) return Math.max(nums[0], nums[1]);
+        if(len == 3) return Math.max(nums[1], nums[2] + nums[0]);
+
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[2] + nums[0];
+
+        for(int i = 3; i < len ; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
+        }
+
+        return Math.max(dp[len - 1], dp[len-2]);
+    }
+}

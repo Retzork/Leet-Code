5451db58ca30f559727a4196fc7b755760e132d9
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-23 16:12:34 +0700

    Create 22nd.java

---
+++ 2024/January/22nd.java
@@ -0,0 +1,17 @@
+class 22nd {
+    public int[] findErrorNums(int[] nums) {
+        int len = nums.length, missing = 0, dupe = 0;
+        int[] arr = new int[len + 1];
+
+        for(int i = 0; i < len; i++){
+            arr[nums[i]]++;
+        }
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] == 2) dupe = i;
+            if(arr[i] == 0) missing = i;
+        }
+
+        return new int[] {dupe, missing};
+    }
+}

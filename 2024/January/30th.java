1252ab50187d09e3181c07815fb230e86bc4440f
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-30 11:15:42 +0700

    Create 30th.java

---
+++ 2024/January/30th.java
@@ -0,0 +1,31 @@
+class 30th {
+    public int evalRPN(String[] tokens) {
+        List<Integer> list = new ArrayList<>();
+
+        for (String token : tokens) {
+            try {
+                int elem = Integer.parseInt(token);
+                list.add(elem);
+            } catch (NumberFormatException e) {
+                int operand2 = list.remove(list.size() - 1);
+                int operand1 = list.remove(list.size() - 1);
+                list.add(operate(operand1, operand2, token));
+            }
+        }
+
+        return list.get(0);
+    }
+
+    public int operate(int op1, int op2, String op){
+        switch (op) {
+            case "-":
+                return op1 - op2;
+            case "*":
+                return op1 * op2;
+            case "/":
+                return op1 / op2;
+            default :
+                return op1 + op2;
+        }
+    }
+}

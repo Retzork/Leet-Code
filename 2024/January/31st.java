67abc983d05056f1da8090e22768c77ff5986a55
Retzork <mfauzialinsyraq77@gmail.com>
2024-01-31 17:29:12 +0700

    Create 31st.java

---
+++ 2024/January/31st.java
@@ -0,0 +1,19 @@
+class 31st {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] result = new int[len];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = len - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}

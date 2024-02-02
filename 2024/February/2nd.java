286dd661beb313e35b094945c1776f2813d6b1e7
Retzork <mfauzialinsyraq77@gmail.com>
2024-02-02 17:15:34 +0700

    Create 2nd.java

---
+++ 2024/February/2nd.java
@@ -0,0 +1,18 @@
+class 2nd {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> numbersList = new ArrayList<>();
+        String allDigits = "123456789";
+
+        for (int length = String.valueOf(low).length(); length <= String.valueOf(high).length(); length++) {
+            for (int start = 0; start <= 9 - length; start++) {
+                String subString = allDigits.substring(start, start + length);
+                int num = Integer.parseInt(subString);
+                if (num >= low && num <= high) {
+                    numbersList.add(num);
+                }
+            }
+        }
+
+        return numbersList;
+    }
+}
